bcfeeee7c0c647b5379d394b1eda283bcdf19127
pluto.bing.liu <pluto.bing.liu@gmail.com>
2013-10-28 11:16:09 +0800

    修改ProtocolConfig的内容

--- musketeer-datasync/datasync-protocol/datasync-protocol-core/src/main/java/net/musketeer/datasync/protocol/config/ProtocolConfig.java
+++ musketeer-datasync/datasync-protocol/datasync-protocol-core/src/main/java/net/musketeer/datasync/protocol/config/ProtocolConfig.java
@@ -1,17 +1,80 @@
-/*******************************************************************************
- * Copyright (c) 2013 JD Corporation and others.
- * 
- * Contributors:
- *     JD Corporation 
- *******************************************************************************/
 package net.musketeer.datasync.protocol.config;
 
-/**
- *
- * <p></p>
- * @author liubing
- * Date Oct 28, 2013
- */
-public class ProtocolConfig {
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name = "protocol")
+public abstract class ProtocolConfig extends AbstractDefinition {
+	@XmlAttribute
+	protected String id;
+	@XmlAttribute
+	protected ModeEnum mode;
+	@XmlAttribute
+	protected SideEnum side;
+	@XmlAttribute
+	protected IODirection ioDirection;
+	@XmlElementRef
+	protected RequestConfig request;
+	@XmlElementRef
+	protected ResponseConfig response;
+	@XmlElementRef
+	protected CommonsConfig commons;
+
+	public RequestConfig getRequest() {
+		return request;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId( String id ) {
+		this.id = id;
+	}
+
+	public ModeEnum getMode() {
+		return mode;
+	}
+
+	public void setMode( ModeEnum mode ) {
+		this.mode = mode;
+	}
+
+	public SideEnum getSide() {
+		return side;
+	}
+
+	public void setSide( SideEnum side ) {
+		this.side = side;
+	}
+
+	public IODirection getIoDirection() {
+		return ioDirection;
+	}
+
+	public void setIoDirection( IODirection ioDirection ) {
+		this.ioDirection = ioDirection;
+	}
+
+	public void setRequest( RequestConfig request ) {
+		this.request = request;
+	}
+
+	public ResponseConfig getResponse() {
+		return response;
+	}
+
+	public void setResponse( ResponseConfig response ) {
+		this.response = response;
+	}
+
+	public CommonsConfig getCommons() {
+		return commons;
+	}
+
+	public void setCommons( CommonsConfig commons ) {
+		this.commons = commons;
+	}
 
 }
